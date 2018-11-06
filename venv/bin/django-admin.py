84d5e6a9856e9b2fa9164cd1fff0d916012d3d05
jagrutipatel199320 <jagruti.patel199320@gmail.com>
2018-11-06 16:45:20 -0500

    firt commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jagrutibhudiya/Desktop/python/Django/Visulization/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
